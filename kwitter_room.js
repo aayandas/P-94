3957a44b34ded210282d9df3e1892a37a2c69d2a
aayandas <94917206+aayandas@users.noreply.github.com>
2022-03-13 23:08:42 +0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5cPIFeB55VjVer74wTG5fyzviphzS-nM",
+    authDomain: "p-94-kwitter.firebaseapp.com",
+    projectId: "p-94-kwitter",
+    storageBucket: "p-94-kwitter.appspot.com",
+    messagingSenderId: "476271709282",
+    appId: "1:476271709282:web:fd2d88302bfe1aaa99153b"
+  };
